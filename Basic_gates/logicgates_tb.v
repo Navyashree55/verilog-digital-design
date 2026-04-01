d86ae42e0da49a9104867588c71a6866f678a0be
Navyashree R <gnavya923@gmail.com>
2026-04-01 21:08:04 +0530

    Update logicgates_tb.v

--- Basic_gates/logicgates_tb.v
+++ Basic_gates/logicgates_tb.v
@@ -1,16 +1,24 @@
 module tb_all_gates;
-  wire A,B;
-  reg  X,Y,Z,R,T,S;
-  all_gates dut(A,B,X,Y,Z,R,T,S);
+
+  reg A, B;
+  wire X, Y, Z, W, R, T, S;
+
+  all_gates dut(A, B, X, Y, Z, W, R, T, S);
+
   initial begin
     $dumpfile("dump.vcd");
-    $dumpvars(1);
-    #1 A = 0;  B=0;
-    #1 A = 0;  B=1;
-    #1 A = 1;  B=0;
-    #1 A = 1;  B=1;
-    $monitor("A=%b B=%b | X=%b Y=%b Z=%b R=%b T=%b S=%b ",A,B,X,Y,Z,R,T,S);
-  end 
+    $dumpvars(0, tb_all_gates);
+
+    $monitor("A=%b B=%b | X=%b Y=%b Z=%b W=%b R=%b T=%b S=%b",
+              A, B, X, Y, Z, W, R, T, S);
+
+    // Apply inputs with delays
+    A = 0; B = 0;
+    #5 A = 0; B = 1;
+    #5 A = 1; B = 0;
+    #5 A = 1; B = 1;
+
+    #5 $finish;   // delay before finishing
+  end
+
 endmodule
-    
-    
